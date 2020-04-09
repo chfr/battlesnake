8a5032cb91a91a4a9f1344b19b2f2cab6fee3ede
Christoffer Friberg <christoffer.friberg@trioptima.com>
2020-04-09 16:19:09 +0200

    Always go up

--- src/main/kotlin/net/chfr/snake/snake.kt
+++ src/main/kotlin/net/chfr/snake/snake.kt
@@ -1,10 +1,22 @@
 package net.chfr.snake
 
 import io.javalin.Javalin
+import io.javalin.http.Context
 
 fun main(args: Array<String>) {
     val app = Javalin.create().start(getHerokuAssignedPort())
     app.get("/") { ctx -> ctx.result("Hello herokuuuu") }
+    app.post("/ping") { ctx -> ctx.result("") }
+    app.post("/start") { ctx -> ctx.result("") }
+    app.post("/end") { ctx -> ctx.result("") }
+    app.post("/move") { ctx -> move(ctx) }
+}
+
+fun move(context: Context) {
+    val response = mapOf(
+        "move" to "up"
+    )
+    context.json(response)
 }
 
 fun getHerokuAssignedPort(): Int {
