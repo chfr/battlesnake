7875099c5c3a197a71088ee0ca2994187b825ab9
Christoffer Friberg <christoffer.friberg@trioptima.com>
2020-04-09 16:02:36 +0200

    Listen to Heroku-defined port rather than hardcoding 7000

--- src/main/kotlin/net/chfr/snake/snake.kt
+++ src/main/kotlin/net/chfr/snake/snake.kt
@@ -3,6 +3,11 @@ package net.chfr.snake
 import io.javalin.Javalin
 
 fun main(args: Array<String>) {
-    val app = Javalin.create().start(7000)
-    app.get("/") { ctx -> ctx.result("Hello World") }
+    val app = Javalin.create().start(getHerokuAssignedPort())
+    app.get("/") { ctx -> ctx.result("Hello herokuuuu") }
+}
+
+fun getHerokuAssignedPort(): Int {
+    val herokuPort = System.getenv("PORT")
+    return herokuPort?.toInt() ?: 7000
 }
